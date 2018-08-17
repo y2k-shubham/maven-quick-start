3fb8574e89c9a9fe40e33f348b4a652cd5068aa9
y2k-shubham <y2k.shubhamgupta@gmail.com>
2018-08-17 11:31:30 +0530

    Change compiler-plugin (source / target) to Java 1.8

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,14 +1,28 @@
 package clinic.programming.training;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     
     public Application() {
         System.out.println ("Inside Application");
     }
 
+    public void greet() {
+    	List <String> greetings = new ArrayList<>();
+    	greetings.add("Hello!");
+
+    	for (String greeting: greetings) {
+    		System.out.println("Greeting: " + greeting);
+    	}
+    }
+
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 		Application app = new Application();
+
+		app.greet();
     }
 }
